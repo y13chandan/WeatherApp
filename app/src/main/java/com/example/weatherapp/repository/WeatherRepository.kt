8451224430c60aec474c2c5f910f9763438dabab
chandan <chandan.singh@industrybuying.com>
2021-04-12 11:29:03 +0530

    view complete

--- app/src/main/java/com/example/weatherapp/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherapp/repository/WeatherRepository.kt
@@ -5,6 +5,5 @@ import com.example.weatherapp.network.ApiService
 import javax.inject.Inject
 
 class WeatherRepository @Inject constructor(private val apiService: ApiService) {
-
     suspend fun getWeatherData(accessKey: String, cityName: String): WeatherResponse = apiService.getWeatherData(accessKey, cityName)
 }
